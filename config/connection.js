bba9c64fc2e834fd8962db027e74ce48a3d8b905
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-05-20 01:30:50 -0400

    product crud tests passed

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+// import the sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+
+// create a connection to our database
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+  });
+    
+//   const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
+
+module.exports = sequelize;
---
+++ models/ProductTag.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the products models id
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references the tag models id
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
